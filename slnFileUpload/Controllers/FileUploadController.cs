937e8b4a5d7f2acff0b60c01759903e9651dff83
karta571110 <44608844+karta571110@users.noreply.github.com>
2020-03-27 18:52:43 +0800

    加入專案檔案。

---
+++ slnFileUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace slnFileUpload.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase photo)
+        {
+            string fileName = string.Empty;//上傳圖檔
+
+            if(photo != null)
+            {
+                if (photo.ContentLength > 0)
+                {
+                    fileName = Path.GetFileName(photo.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Photos"), fileName);
+                    photo.SaveAs(path);
+                }
+            }
+            return RedirectToAction("ShowPhotos");
+        }
+
+        /// <summary>
+        /// ShowPhotos方法可顯示Photos資料夾下的所有圖檔
+        /// </summary>
+        /// <returns></returns>
+        public string ShowPhotos()
+        {
+            string show = string.Empty;
+
+            DirectoryInfo dir = new DirectoryInfo(Server.MapPath("~/Photos"));
+
+            FileInfo[] finfo = dir.GetFiles();//取得dir物件下的所有檔案(即Photos資料夾下)並放入finfo檔案資訊陣列
+            int n = 0;
+
+            foreach(FileInfo result in finfo)
+            {
+                n++;
+
+                show += "<a href='../Photos/" + result.Name + "'target='_blank'><img src='../Photos/" + result.Name + "' width='90' heigth='60' border='0'></a>";
+                if (n % 4 == 0)//顯示四個圖之後即跳一段落
+                {
+                    show += "<P>";
+
+                }
+
+            }
+
+            show += "<p><a href='Create'>返回</a></p>";
+            return show;
+
+        }
+    }
+}
---
+++ slnFileUpload/Controllers/MultiFileUploadController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace slnFileUpload.Controllers
+{
+    public class MultiFileUploadController : Controller
+    {
+        // GET: MultiFileUpload
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase[] photos)
+        {
+            string fname = string.Empty;
+
+            for(int i = 0; i < photos.Length; i++)
+            {
+                HttpPostedFileBase f = (HttpPostedFileBase)photos[i];
+
+                if (f != null)
+                {
+                    //取得上傳的檔案名稱
+                    fname = f.FileName.Substring(f.FileName.LastIndexOf("\\") + 1);
+
+                    //將檔案儲存到網站的files資料夾下
+                    f.SaveAs(Server.MapPath("~/Photos") + "\\" + fname);
+                }
+            }
+            return RedirectToAction("ShowPhotos");
+        }
+
+        //ShowPhotos方法可顯示Photos資料夾下所有圖檔
+        public string ShowPhotos()
+        {
+            string show = string.Empty;
+
+            //建立可操作Photos資料夾的dir物件
+            DirectoryInfo dir = new DirectoryInfo(Server.MapPath("~/Photos"));
+            //取得dir物件下的所有檔案(即Photo資料夾下)並放入finfo檔案資訊陣列
+            FileInfo[] fInfo = dir.GetFiles();
+            int n = 0;
+            //逐一將finfo檔案資訊陣列內的所有圖檔指定給show變數
+            foreach(FileInfo result in fInfo)
+            {
+                n++;
+                //將目前取得的圖指定給show字串
+                show += "<a href='../Photos/" + result.Name + "'target='_blank'><img src='../Photos/" + result.Name + "'width='90' height='60' border='0'></a> ";
+                if (n % 4 == 0)
+                {
+                    show += "<p>";
+                }
+            }
+            //show變數再加上 '返回' Create 動作方法的連結
+            show += "<p><a href='Create'>返回</a></p>";
+            return show;
+
+        }
+    }
+}
